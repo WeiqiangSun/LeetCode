05ce2278fc847bae85f6809e7295a0435372e744
sunweiqiang <sunweiqiang@kuaishou.com>
2018-06-02 17:52:53 +0800

    Revert "3"
    This reverts commit c7fbe51
    
    Revert "2"
    
    This reverts commit ecf3a01
    
    Revert "1"
    
    This reverts commit ea0313b

--- src/java/leetcode/_3_LongestSubstringWithoutRepeatingCharacters.java
+++ src/java/leetcode/_3_LongestSubstringWithoutRepeatingCharacters.java
@@ -51,9 +51,6 @@ public class _3_LongestSubstringWithoutRepeatingCharacters {
             }
         }
 
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
         return max;
     }
 }

66bae457fc7c05604fc6f6a3dcd0c989f833ff72
sunweiqiang <sunweiqiang@kuaishou.com>
2018-07-17 19:50:44 +0800

    2

--- src/java/leetcode/_3_LongestSubstringWithoutRepeatingCharacters.java
+++ src/java/leetcode/_3_LongestSubstringWithoutRepeatingCharacters.java
@@ -54,6 +54,7 @@ public class _3_LongestSubstringWithoutRepeatingCharacters {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
+        System.out.println(4);
         return max;
     }
 }
